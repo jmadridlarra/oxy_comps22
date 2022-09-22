aa2259ea2e15cb0fa61a8def3b250d0eaf21fbe9
Joaquin <joaquinemilio7@gmail.com>
2022-09-22 15:59:47 -0700

    adding beginnings of textures, moved into adding movement dependancies

--- open_gl_testing.cpp
+++ open_gl_testing.cpp
@@ -4,6 +4,15 @@
 #include <C:/Users/joaqu/source/repos/openGL_testing/openGL_testing/shader_s.h> // use our in house shader
 //C:\Users\joaqu\source\repos\openGL_testing\openGL_testing\shader_s.h
 
+// for image textures
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// for movement
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -52,7 +61,7 @@ int main()
 
     // build and compile our shader program
     // ------------------------------------
-    Shader ourShader("my_shader.vs", "my_shader.fs"); // you can name your shader files however you like
+    Shader ourShader("../../../../OneDrive/Desktop/comps/my_shader.vs", "C:/Users/joaqu/OneDrive/Desktop/comps/my_shader.fs"); // you can name your shader files however you like
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -63,8 +72,24 @@ int main()
          0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
     };
 
-    // first two args define the lower left corner
-    // last two args length and width of the window
+    // a texture is an image, this assigns the 
+    float texCoords[] = {
+    0.0f, 0.0f,  // lower-left corner  
+    1.0f, 0.0f,  // lower-right corner
+    0.5f, 1.0f   // top-center corner
+    };
+    
+
+    // sets repetition behavior
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // scaling behavior
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // depth behavior
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // registering our callback function
     /*
@@ -110,6 +135,7 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     */
+    /*
     //triangle
     float vertices_tri[] = {
      0.5f,  0.5f, 0.0f,  // top right
@@ -121,7 +147,7 @@ int main()
         0, 1, 3,   // first triangle
         1, 2, 3    // second triangle
     };
-
+    */
     
     unsigned int VBO; // vertex buffer object - send in large amounts of data since sending is costly
     glGenBuffers(1, &VBO);
@@ -144,15 +170,20 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    /*
     unsigned int EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
+    */
 
     // 3. then set our vertex attributes pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // 4. color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     // render loop
     while (!glfwWindowShouldClose(window))
     {
@@ -166,8 +197,7 @@ int main()
         // draws triangle        
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // check and call events and swap the buffers
         glfwPollEvents();
